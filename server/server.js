a5ac309b5542f404750e7c097590db5bace2497b
Neequaye <nxn3661@d25sp182gq17im.amer.homedepot.com>
2018-03-16 10:03:21 -0400

    changed port to 8080

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 var bodyParser = require('body-parser')
 const app = express()
-const port = 3001
+const port = 8080
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
