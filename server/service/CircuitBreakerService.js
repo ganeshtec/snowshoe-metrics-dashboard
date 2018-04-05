10bf6ca4faddc3cbd37c538d266e22bee3b05862
Woods, Kevin P <KEVIN_P_WOODS@homedepot.com>
2018-04-05 11:39:32 -0400

    Feature/update circuit breaker info with cloud and store calls/156343104 (#10)
    * [156437874] fixed dupilcate values for markdown errors
    
    * [156343104] added full info
    
    * [156343104] adding additionall data to circuit breaker section

--- server/service/CircuitBreakerService.js
+++ server/service/CircuitBreakerService.js
@@ -25,44 +25,43 @@ processCircuitBreakerSplunkResponse = (error, results) => {
         {
             description: "Percentage of Total Calls Meeting SLA: ",
             count: Math.round((values[4] / values[0]) * 10000)/100 + " %"
-        }
-        // {
-        //     description: "Average Response Time of Cloud Calls: ",
-        //     count: Math.round(values[6]) + " ms"
-        // },
-        // {
-        //     description: "Shortest Response Time of Cloud Calls: ",
-        //     count: values[7] + " ms"
-        // },
-        // {
-        //     description: "Longest Response Time of Cloud Calls: ",
-        //     count: values[8] + " ms"
-        // },
-        // {
-        //     description: "Percentage of Cloud Calls Meeting SLA: ",
-        //     count: Math.round((values[9] / values[5]) * 10000)/100 + " %"
-        // },
-        // {
-        //     description: "Cloud Percentage of Calls w/ Discount Returned: ",
-        //     count: Math.round((values[10] / values[5]) * 10000)/100 + " %"
-        // },
-        // {
-        //     description: "Average Response Time of Store Calls: ",
-        //     count: Math.round(values[12]) + " ms"
-        // },
-        // {
-        //     description: "Shortest Response Time of Store Calls: ",
-        //     count: values[13] + " ms"
-        // },
-        // {
-        //     description: "Longest Response Time of Store Calls: ",
-        //     count: values[14] + " ms"
-        // },
-        // {
-        //     description: "Percentage of Store Calls Meeting SLA: ",
-        //     count: Math.round((values[15] / values[11]) * 10000)/100 + " %"
-        // }
-
+        },
+        {
+            description: "Average Response Time of Cloud Calls: ",
+            count: Math.round(values[6]) + " ms"
+        },
+        {
+            description: "Shortest Response Time of Cloud Calls: ",
+            count: values[7] + " ms"
+        },
+        {
+            description: "Longest Response Time of Cloud Calls: ",
+            count: values[8] + " ms"
+        },
+        {
+            description: "Percentage of Cloud Calls Meeting SLA: ",
+            count: Math.round((values[9] / values[5]) * 10000)/100 + " %"
+        },
+        {
+            description: "Cloud Percentage of Calls w/ Discount Returned: ",
+            count: Math.round((values[10] / values[5]) * 10000)/100 + " %"
+        },
+        {
+            description: "Average Response Time of Store Calls: ",
+            count: Math.round(values[12]) + " ms"
+        },
+        {
+            description: "Shortest Response Time of Store Calls: ",
+            count: values[13] + " ms"
+        },
+        {
+            description: "Longest Response Time of Store Calls: ",
+            count: values[14] + " ms"
+        },
+        {
+            description: "Percentage of Store Calls Meeting SLA: ",
+            count: Math.round((values[15] / values[11]) * 10000)/100 + " %"
+        },
     ]
     return (output)
 }
