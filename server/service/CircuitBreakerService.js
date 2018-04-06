1cb14f4764e11f4b455d0a9f939fc69c7be2ef87
Woods <kpw55bs@d25t4092gq17im.ssc.homedepot.com>
2018-04-06 09:30:59 -0400

    minor changes and adding buildConfig.json

--- server/service/CircuitBreakerService.js
+++ server/service/CircuitBreakerService.js
@@ -7,61 +7,73 @@ processCircuitBreakerSplunkResponse = (error, results) => {
 
     var output  = [
         {
+            description: "--------CIRCUIT BREAKER METRICS--------"
+        },
+        {
             description: "Total Calls Generated",
             count: values[0] + " calls"
         },
         {
-            description: "Average Response Time of Total Calls: ",
+            description: "Circuit Breaker Average Response Time: ",
             count: Math.round(values[1]) + " ms"
         },
         {
-            description: "Shortest Response Time of Total Calls: ",
+            description: "Circuit Breaker Shortest Response Time: ",
             count: values[2] + " ms"
         },
         {
-            description: "Longest Response Time of Total Calls: ",
+            description: "Circuit Breaker Longest Response Time: ",
             count: values[3] + " ms"
         },
         {
-            description: "Percentage of Total Calls Meeting SLA: ",
-            count: Math.round((values[4] / values[0]) * 10000)/100 + " %"
+            description: "--------CLOUD METRICS--------"
         },
         {
-            description: "Average Response Time of Cloud Calls: ",
+            description: "Cloud Average Response Time: ",
             count: Math.round(values[6]) + " ms"
         },
         {
-            description: "Shortest Response Time of Cloud Calls: ",
+            description: "Cloud Shortest Response Time: ",
             count: values[7] + " ms"
         },
         {
-            description: "Longest Response Time of Cloud Calls: ",
+            description: "Cloud Longest Response Time: ",
             count: values[8] + " ms"
+        },
+        {
+            description: "--------STORE METRICS--------"
+        },
+        {
+            description: "Store Average Response Time: ",
+            count: Math.round(values[11]) + " ms"
+        },
+        {
+            description: "Store Shortest Response Time: ",
+            count: values[12] + " ms"
+        },
+        {
+            description: "Store Longest Response Time: ",
+            count: values[13] + " ms"
+        },
+        {
+            description: "--------SLA METRICS--------"
+        },
+        {
+            description: "Circuit Breaker Percentage of calls meeting SLA: ",
+            count: Math.round((values[4] / values[0]) * 10000)/100 + " %"
         },
         {
             description: "Percentage of Cloud Calls Meeting SLA: ",
             count: Math.round((values[9] / values[5]) * 10000)/100 + " %"
         },
         {
-            description: "Cloud Percentage of Calls w/ Discount Returned: ",
-            count: Math.round((values[10] / values[5]) * 10000)/100 + " %"
-        },
-        {
-            description: "Average Response Time of Store Calls: ",
-            count: Math.round(values[12]) + " ms"
-        },
-        {
-            description: "Shortest Response Time of Store Calls: ",
-            count: values[13] + " ms"
-        },
-        {
-            description: "Longest Response Time of Store Calls: ",
-            count: values[14] + " ms"
-        },
-        {
             description: "Percentage of Store Calls Meeting SLA: ",
-            count: Math.round((values[15] / values[11]) * 10000)/100 + " %"
+            count: Math.round((values[14] / values[10]) * 10000)/100 + " %"
         },
+        {
+            description: "Cloud Percentage of Calls w/ Discount Returned: ",
+            count: Math.round((values[15] / values[5]) * 10000)/100 + " %"
+        }
     ]
     return (output)
 }
