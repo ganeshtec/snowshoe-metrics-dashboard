e5f8d3444f0acfeb2166c554c18524ba7e3174d1
Spandan Amanaganti <spandan_amanaganti@homedepot.com>
2018-04-12 15:37:40 -0400

    cosmetic changes.

--- client/src/components/Metric.js
+++ client/src/components/Metric.js
@@ -1,13 +1,13 @@
-// import React from 'react';
-//
-// const Metric = (props) => {
-//     return (
-//         <tr className='row'>
-//             <td>{this.props.metric.description}</td>
-//             <td>{this.props.metric.count}</td>
-//             <td className="padding-horizontal-3">{this.props.metric.tomorrow}</td>
-//             <td className="padding-horizontal-3">{this.props.metric.future}</td>
-//         </tr>
-//     );
-// }
-// export default Metric;
+import React from 'react';
+
+const Metric = (props) => {
+    return (
+        <tr className='row'>
+            <td>{this.props.metric.description}</td>
+            <td>{this.props.metric.count}</td>
+            <td className="padding-horizontal-3">{this.props.metric.tomorrow}</td>
+            <td className="padding-horizontal-3">{this.props.metric.future}</td>
+        </tr>
+    );
+}
+export default Metric;
