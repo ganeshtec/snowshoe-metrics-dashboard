d2eb30ad64d72b68656892d9e2f2a4eecedd45c3
Woods, Kevin P <KEVIN_P_WOODS@homedepot.com>
2018-04-24 13:08:01 -0400

    [156353589] tweeking gulp task (#25)

--- gulpFile.js
+++ gulpFile.js
@@ -1,7 +1,5 @@
 var gulp = require('gulp');
 var run = require('gulp-run-command').default;
 
-gulp.task('test', run('npm run test'), function (done) {
-    console.log("DONE RUNNING TESTS")
-})
+gulp.task('test', run('npm run test'))
 
