79b1b2b36cb28948771a2fda8dadbce8815641ae
Neequaye, Nikko <NIKKO_NEEQUAYE@homedepot.com>
2018-04-27 15:04:22 -0400

    Update gulpFile.js
    update the gulptest.js file

--- gulpFile.js
+++ gulpFile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var run = require('gulp-run-command').default;
 
-gulp.task('globals');
+gulp.task('globals',run('npm run test'));
 
 // gulp.task('test', run('npm run test'))
 gulp.task('test', ['globals'], function (done) {
