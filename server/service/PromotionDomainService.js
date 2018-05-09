92c46eecc254f1944e214c6cd9cc4c775be0f277
Spandan Amanaganti <spandan_amanaganti@homedepot.com>
2018-05-09 15:52:01 -0400

    More metrics

--- server/service/PromotionDomainService.js
+++ server/service/PromotionDomainService.js
@@ -11,7 +11,23 @@ var projectId = 'hd-pricing-dev';
 var client = new monitoring.MetricServiceClient({
     keyFilename: 'config/hd-pricing-dev.json',
 });
+var fetchPromotionDomainServiceMetrics = (startDate, endDate) => {
+    var totalCalls = getTotalNumberOfCalls(startDate, endDate);
+    var onlineCalls = getNumberOfOnlineCalls(startDate, endDate);
+    var averageResponseTime = getAverageResponseTime(startDate,endDate);
+    var shortestResponseTime = getShortestResponseTime(startDate, endDate);
+    var p99ResponseTime = getP99ResponseTime(startDate, endDate);
+    var percentageOfCallsMeetingSLA = getPercentageOfCallsMeetingSLA(startDate, endDate);
 
+    return new Promise(function (resolve, reject) {
+        return Promise.all([totalCalls, onlineCalls,averageResponseTime]).then(results => {
+            resolve(results);
+
+        }).catch(function (err) {
+            reject(err);
+        });
+    });
+};
 function getMetric(startDate, endDate, filter, aggregation, description, extractMetric) {
 
     var request = {
@@ -48,20 +64,12 @@ var extractInt64Value = function (point) {
     return point.int64Value;
 };
 
-
-var fetchPromotionDomainServiceMetrics = (startDate, endDate) => {
-    var totalCalls = getTotalNumberOfCalls(startDate, endDate);
-    var onlineCalls = getNumberOfOnlineCalls(startDate, endDate);
-
-    return new Promise(function (resolve, reject) {
-        return Promise.all([totalCalls, onlineCalls]).then(results => {
-            resolve(results);
-
-        }).catch(function (err) {
-            reject(err);
-        });
-    });
+var extractDoubleValue = function (point) {
+    return point.doubleValue;
 };
+
+
+
 
 function getTotalNumberOfCalls(startDate, endDate) {
     var filter = 'metric.type= "logging.googleapis.com/user/enterprise-pricing-promotion-domain-service-calls"';
@@ -81,6 +89,47 @@ function getNumberOfOnlineCalls(startDate, endDate) {
     };
     var description = "Online Calls";
     return getMetric(startDate, endDate, filter, aggregation, description, extractInt64Value);
+}
+
+function getAverageResponseTime(startDate,endDate){
+    var filter = 'metric.type= "logging.googleapis.com/user/enterprise-pricing-promotion-domain-service-cartResponseTime"';
+    var aggregation = {
+        alignmentPeriod: {seconds: (endDate - startDate) / 1000},
+        perSeriesAligner: 'ALIGN_SUM',
+        crossSeriesReducer: 'REDUCE_MEAN'
+    };
+    var description = "Average Response Time";
+    return getMetric(startDate, endDate, filter, aggregation, description, extractDoubleValue);
+}
+
+function getShortestResponseTime(startDate,endDate){
+    var filter = 'metric.type= "logging.googleapis.com/user/enterprise-pricing-promotion-domain-service-cartResponseTime"';
+    var aggregation = {
+        alignmentPeriod: {seconds: (endDate - startDate) / 1000},
+        perSeriesAligner: 'ALIGN_PERCENTILE_99'
+    };
+    var description = "Shortest Response Time";
+    return getMetric(startDate, endDate, filter, aggregation, description, extractDoubleValue);
+}
+
+function getP99ResponseTime(startDate,endDate){
+    var filter = 'metric.type= "logging.googleapis.com/user/enterprise-pricing-promotion-domain-service-cartResponseTime"';
+    var aggregation = {
+        alignmentPeriod: {seconds: (endDate - startDate) / 1000},
+        perSeriesAligner: 'ALIGN_PERCENTILE_99'
+    };
+    var description = "99 Percentile Response Time";
+    return getMetric(startDate, endDate, filter, aggregation, description, extractDoubleValue);
+}
+
+function getPercentageOfCallsMeetingSLA(startDate,endDate){
+    var filter = 'metric.type= "logging.googleapis.com/user/enterprise-pricing-promotion-domain-service-cartResponseTime"';
+    var aggregation = {
+        alignmentPeriod: {seconds: (endDate - startDate) / 1000},
+        perSeriesAligner: 'ALIGN_PERCENTILE_99'
+    };
+    var description = "% of calls meeting SLA";
+    return getMetric(startDate, endDate, filter, aggregation, description, extractDoubleValue);
 }
 
 module.exports = fetchPromotionDomainServiceMetrics;
