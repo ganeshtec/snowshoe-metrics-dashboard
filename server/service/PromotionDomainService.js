2e2f422b4918d91f688578b4a24da86512e2e46e
Spandan Amanaganti <spandan_amanaganti@homedepot.com>
2018-05-10 17:43:31 -0400

    Commit before leave.

--- server/service/PromotionDomainService.js
+++ server/service/PromotionDomainService.js
@@ -6,9 +6,8 @@ var request = require('request');
 var monitoring = require('@google-cloud/monitoring');
 var BigQuery = require('@google-cloud/bigquery');
 var projectId = 'hd-pricing-dev';
+var percentile = require('percentile');
 
-
-// Creates a client
 var client = new monitoring.MetricServiceClient({
     keyFilename: 'config/hd-pricing-dev.json',
 });
@@ -24,68 +23,16 @@ var fetchPromotionDomainServiceMetrics = (startDate, endDate) => {
     var onlineCalls = getNumberOfOnlineCalls(startDate, endDate);
     var averageResponseTime = getAverageResponseTime(startDate, endDate);
     var shortestResponseTime = getShortestResponseTime(startDate, endDate);
-    // var p99ResponseTime = getP99ResponseTime(startDate, endDate);
+    var p99ResponseTime = getP99ResponseTime(startDate, endDate);
     var percentageOfCallsMeetingSLA = getPercentageOfCallsMeetingSLA(startDate, endDate);
 
     return new Promise(function (resolve, reject) {
-        return Promise.all([totalCalls, onlineCalls, averageResponseTime, shortestResponseTime, percentageOfCallsMeetingSLA]).then(results => {
+        return Promise.all([totalCalls, onlineCalls, averageResponseTime, shortestResponseTime, p99ResponseTime, percentageOfCallsMeetingSLA]).then(results => {
             resolve(results);
-
         }).catch(function (err) {
             reject(err);
         });
     });
-};
-
-var fetchPromotionDomainServiceMetricsFromBigQuery = (startDate, endDate, description, options) => {
-
-    return new Promise((resolve, reject) => {
-        bigQuery.createJob(options).then(function (data) {
-            var job = data[0];
-            job.getQueryResults().then(function (rows, error) {
-
-                if (error) {
-                    console.error(error);
-                    reject(error);
-                }
-                var innerList = rows[0];
-                resolve({"description": description, "count": innerList[0].f0_});
-            });
-        }).catch(err => {
-            console.error(err, 'ERROR calling Big Query');
-            reject(err);
-        });
-    })
-};
-
-
-var fetchAverageResponseTime = (startDate, endDate, options) => {
-    console.log(options, 'OPTIONS');
-    return new Promise((resolve, reject) => {
-        bigQuery.createJob(options).then(function (data) {
-            console.log(data, 'DATA');
-            var job = data[0];
-            job.getQueryResults().then(function (rows, error) {
-
-                if (error) {
-                    console.error(error);
-                    reject(error);
-                }
-
-                var innerList = rows[0];
-                var sum = 0;
-                for (var i = 0; i < innerList.length; i++) {
-                    sum = sum + parseInt(innerList[i].jsonPayload_response_time);
-                }
-
-                console.log(sum / innerList.length, 'Avge of Response time');
-                resolve({"description": "Average Response Time", "count": sum / innerList.length});
-            });
-        }).catch(err => {
-            console.error(err, 'ERROR');
-            reject(err);
-        });
-    })
 };
 
 function getMetric(startDate, endDate, filter, aggregation, description, extractMetric) {
@@ -148,7 +95,29 @@ function getAverageResponseTime(startDate, endDate) {
         }
     };
 
-    return fetchAverageResponseTime(startDate, endDate, options);
+    return new Promise((resolve, reject) => {
+        bigQuery.createJob(options).then(function (data) {
+            console.log(data, 'DATA');
+            var job = data[0];
+            job.getQueryResults().then(function (rows, error) {
+
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                }
+
+                var innerList = rows[0];
+                var sum = 0;
+                for (var i = 0; i < innerList.length; i++) {
+                    sum = sum + parseInt(innerList[i].jsonPayload_response_time);
+                }
+                resolve({"description": "Average Response Time", "count": sum / innerList.length});
+            });
+        }).catch(err => {
+            console.error(err, 'ERROR');
+            reject(err);
+        });
+    })
 };
 
 
@@ -161,8 +130,22 @@ function getShortestResponseTime(startDate, endDate) {
         }
     };
 
-    let description = "Shortest Response Time";
-    return fetchPromotionDomainServiceMetricsFromBigQuery(startDate, endDate, description, options);
+    return new Promise((resolve, reject) => {
+        bigQuery.createJob(options).then(function (data) {
+            var job = data[0];
+            job.getQueryResults().then(function (rows, error) {
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                }
+                var innerList = rows[0];
+                resolve({"description": "Shortest Response Time", "count": innerList[0].f0_});
+            });
+        }).catch(err => {
+            console.error(err, 'ERROR calling Big Query');
+            reject(err);
+        });
+    })
 }
 
 function getP99ResponseTime(startDate, endDate) {
@@ -170,15 +153,31 @@ function getP99ResponseTime(startDate, endDate) {
     const options = {
         configuration: {
             query: {
-                query: `SELECT count(jsonPayload.response_time) FROM [hd-pricing-dev:exported_logs_v2.promotion_domain_svc_access_log_structured_20180510] WHERE timestamp>'2018-05-01T07:00:00.000Z' AND timestamp<'2018-05-11T06:59:59.000Z' AND jsonPayload.response_time < '200'`
+                query: `SELECT jsonPayload.response_time FROM [hd-pricing-dev:exported_logs_v2.promotion_domain_svc_access_log_structured_20180510] WHERE timestamp>'2018-05-01T07:00:00.000Z' AND timestamp<'2018-05-11T06:59:59.000Z' `
             }
         }
     };
 
-    var description = "99 Percentile Response Time";
-    return fetchPromotionDomainServiceMetricsFromBigQuery(startDate, endDate, description, options);
+    return new Promise((resolve, reject) => {
+        bigQuery.createJob(options).then(function (data) {
+            var job = data[0];
+            job.getQueryResults().then(function (rows, error) {
+                if (error) {
+                    console.error(error);
+                    reject(error);
+                }
+                var innerList = rows[0];
+                resolve({
+                    "description": "99 Percentile Response Time",
+                    "count": innerList[0].jsonPayload_response_time
+                });
+            });
+        }).catch(err => {
+            console.error(err, 'ERROR calling Big Query');
+            reject(err);
+        });
+    })
 }
-
 
 function getPercentageOfCallsMeetingSLA(startDate, endDate) {
 
@@ -211,10 +210,8 @@ function getPercentageOfCallsMeetingSLA(startDate, endDate) {
                 }
                 var innerList = rows[0];
                 queryResults = innerList[0].f0_;
-                console.log(queryResults, '$##$#$#$#$#$#$');
             });
         }).catch(err => {
-            console.error(err, 'ERROR calling Big Query');
             reject(err);
         });
 
@@ -229,10 +226,8 @@ function getPercentageOfCallsMeetingSLA(startDate, endDate) {
                 var innerList = rowsTwo[0];
                 queryResultsTwo = innerList[0].f0_;
                 resolve({"description": "% Of calls meeting SLA", "count": queryResults / queryResultsTwo * 100});
-                console.log(queryResultsTwo, '$##$#$#$#$#$#$');
             });
         }).catch(err => {
-            console.error(err, 'ERROR calling Big Query');
             reject(err);
         });
     })
